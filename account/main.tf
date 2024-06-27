b7ff138be6a8c3e0f8efcb36785a27497c5d5035
moulayhafidelhafa <myhafid.myelhafa@gmail.com>
2024-06-27 16:38:08 +0000

    edit main.tf

--- account/main.tf
+++ account/main.tf
@@ -1,18 +1,15 @@
-terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = ">= 5.35.0"
+data "google_billing_account" "account" {
+  billing_account = var.billing_account_id == null ? null : "billingAccounts/${var.billing_account_id}"
+  display_name    = var.billing_account_name
+
+  lifecycle {
+    precondition {
+      condition     = var.billing_account_id != null || var.billing_account_name != null
+      error_message = "One of billing_account_id or billing_account_name must be specified."
     }
-    external = {
-      source  = "hashicorp/external"
-      version = ">= 3.2.2"
+    precondition {
+      condition     = (var.billing_account_id != null) != (var.billing_account_name != null)
+      error_message = "Only one of billing_account_id or billing_account_name may be specified."
     }
   }
-}
-
-provider "google" {
-  billing_project       = var.billing_project_id
-  project               = var.billing_project_id
-  user_project_override = true
 }
